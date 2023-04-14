17fa9827ee2a2631a41180483c3fe234987ef1d1
Lexpiper <lexpiperfx@gmail.com>
2023-04-14 10:35:49 +0100

    header file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*LIBRARIES*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+/*MACROS*/
+#define MAX_INPUT_LENGTH 1024
+
+
+#endif
